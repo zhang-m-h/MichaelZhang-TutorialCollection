81ab380bb5f52724eebbbec6234dc4ac2da750c3
unknown <michael34916@gmail.com>
2024-03-18 22:26:41 -0400

    Second Commit

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const decreaseButt = document.getElementById("decreaseButt");
 const resetButt = document.getElementById("resetButt");
 const increaseButt = document.getElementById("increaseButt");
+const randomButt = document.getElementById("randButt");
 const countLabel = document.getElementById("countLabel");
 
 const submitButt = document.getElementById("submitButt");
 const stepLabel = document.getElementById("stepLabel");
 const curStep = document.getElementById("step");
 
+// some constants for random button
+const max = 100;
+const min = 0;
+
+// initialize current number and step to increase/decrease by
 let count = 0;
 let step = 1;
 
@@ -24,6 +30,11 @@ resetButt.onclick = function(){
     count = 0;
     step = 1;
     curStep.textContent = 1;
+    countLabel.textContent = count;
+}
+
+randomButt.onclick = function(){
+    count = Math.round(Math.random() * max) + min;
     countLabel.textContent = count;
 }
 
